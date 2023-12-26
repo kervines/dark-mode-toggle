ff8f3ee01c35d46da711f4a53bc7521fe0afbcab
kervines <kervinzero@gmail.com>
2023-12-26 19:49:56 -0300

    feat - adicionado funçao que ativa o dark mode

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const circule = document.querySelector('.circule');
+const sun = document.querySelector('.sun');
+const moon = document.querySelector('.moon');
+const html = document.querySelector('html');
+const container = document.querySelector('.container');
+
+container.addEventListener('click', activeClass);
+function activeClass() {
+  moon.classList.toggle('active');
+  sun.classList.toggle('active');
+  circule.classList.toggle('active');
+  const mem = html.classList.toggle('dark-mode');
+  localStorage.setItem('mode', mem);
+}
+
+const mem = JSON.parse(localStorage.getItem('mode'));
+if (mem) {
+  activeClass();
+}
